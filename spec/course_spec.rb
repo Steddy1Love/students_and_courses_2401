78c6dcb6ada1629e03d8814ba758abc3f096984e
Steddman Bell <steddfast@gmail.com>
2024-02-14 08:34:58 -0800

    Creates first three tests for course class

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -2,4 +2,23 @@ require 'rspec'
 require './lib/course'
 require './lib/student'
 
+RSpec.describe Course do
+    it 'exists' do
+        course = Course.new("Calculus", 2)
+        expect(course).to be_instance_of(Course)
+    end
+
+    it 'has attributes' do
+        course = Course.new("Calculus", 2)
+        expect(course.name).to eq("Calculus")
+        expect(course.capacity).to eq(2)
+        expect(course.students).to eq([])
+    end
+
+    it 'tests if full' do
+        course = Course.new("Calculus", 2)
+        expect(course.full?).to be false
+    end
+end
+
 
