e3fcdd036727089771661fe02e9aeab4e41a728d
Steddman Bell <steddfast@gmail.com>
2024-02-14 08:40:22 -0800

    Passed first three tests of course spec file

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,18 @@
 class Course
- 
+    attr_reader :name, :capacity, :students
+
+    def initialize(name, capacity)
+        @name = name
+        @capacity = capacity
+        @students = []
+    end
+
+    def full?
+        @capacity == 0
+    end
+
+    def enroll(student_num)
+        @students << student_num
+        @capacity -= 1
+    end
 end
