6dde4bbd0605451385af2b1dfd4049abfdefa1ff
Steddman Bell <steddfast@gmail.com>
2024-02-14 08:17:30 -0800

    First three tests for student spec file passed

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,9 @@
 class Student
-    attr_reader :name, :age
+    attr_reader :name, :age, :scores
 
     def initialize(student_hash)
         @name = student_hash[:name]
         @age = student_hash[:age]
+        @scores = []
     end
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -7,5 +7,14 @@ RSpec.describe Student do
     expect(morgan).to be_instance_of(Student)
   end
 
-  
+  it 'has attributes' do
+    morgan = Student.new({name: "Morgan", age: 21})
+    expect(morgan.name).to eq("Morgan")
+    expect(morgan.age).to eq(21)
+  end
+
+  it 'has a scores array' do
+    morgan = Student.new({name: "Morgan", age: 21})
+    expect(morgan.scores).to eq([])
+  end
 end
